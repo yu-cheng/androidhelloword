4cd1d284034240a593055ed5615a1fece3ab5c97
c-cc <946271976@qq.com>
2017-04-11 11:00:08 +0800

    说明

---
+++ app/src/main/java/com/example/maibenben/myapplication/hellowordMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.maibenben.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hellowordMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_word_layout);
+    }
+}
